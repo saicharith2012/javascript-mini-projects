376182d1012deddbff2dbd26b4421a30c363579b
saicharith2012 <saicharithpalanbindhela@gmail.com>
2022-03-10 13:17:27 +0530

    added a feature where the passenger count is always shown total

--- PassengerCounterApp/script.js
+++ PassengerCounterApp/script.js
@@ -6,10 +6,14 @@ let countEl = document.getElementById("count-el") // passing in arguments
 let saveEl = document.getElementById("save-el")
 
 // console.log(countEl)
+let numberOfClicks = 0
 
 // listen for clicks on the increment button
 function increment(){
     console.log('New passengers arrived...Welcome!')
+
+    numberOfClicks += 1
+    
     // increment the count variable when the button is clicked
     count += 1
     countEl.innerText = count
@@ -17,10 +21,9 @@ function increment(){
 }
 
 function save(){
-    let saveMessage = count + ' - '
+    let saveMessage = numberOfClicks + ' - '
     saveEl.textContent += saveMessage
     console.log(count)
-    //setting back the count to 0 after saving the prev count
-    count = 0
-    countEl.innerText = count
+    numberOfClicks = 0;
+    
 }
