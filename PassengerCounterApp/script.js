5c2ad20910a47222c088a31deff63f7d0e88bd59
saicharith2012 <saicharithpalabindhela@gmail.com>
2022-03-29 19:03:27 +0530

    added clear button

--- PassengerCounterApp/script.js
+++ PassengerCounterApp/script.js
@@ -1,29 +1,37 @@
-
 // intialize the count as 0
-let count = 0
+let count = 0;
 
-let countEl = document.getElementById("count-el") // passing in arguments
-let saveEl = document.getElementById("save-el")
+let countEl = document.getElementById("count-el"); // passing in arguments
+let saveEl = document.getElementById("save-el");
 
 // console.log(countEl)
-let numberOfClicks = 0
+let numberOfClicks = 0;
 
 // listen for clicks on the increment button
-function increment(){
-    console.log('New passengers arrived...Welcome!')
+function increment() {
+  console.log("New passengers arrived...Welcome!");
 
-    numberOfClicks += 1
-    
-    // increment the count variable when the button is clicked
-    count += 1
-    countEl.innerText = count
-    // console.log(count)
+  numberOfClicks += 1;
+
+  // increment the count variable when the button is clicked
+  count += 1;
+  countEl.innerText = count;
+  // console.log(count)
 }
 
-function save(){
-    let saveMessage = numberOfClicks + ' - '
-    saveEl.textContent += saveMessage
-    console.log(count)
-    numberOfClicks = 0;
-    
+// saving the count
+function save() {
+  let saveMessage = numberOfClicks + " | ";
+  saveEl.textContent += saveMessage;
+  console.log(count);
+  numberOfClicks = 0;
+  //   console.log(saveMessage);
+}
+
+// clears all the count and entries
+function clearAll() {
+  count = 0;
+  countEl.textContent = count;
+  console.log(count);
+  saveEl.textContent = "Previous Entries: ";
 }
