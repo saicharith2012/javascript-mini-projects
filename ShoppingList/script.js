4f5b60204a417eafc21cd69261c5883d6be331b3
saicharith2012 <saicharithpalabindhela@gmail.com>
2022-03-30 07:25:50 +0530

    final commit

--- ShoppingList/script.js
+++ ShoppingList/script.js
@@ -1,37 +1,39 @@
-// var button = document.getElementsByTagName("button")[0];
+console.log("Welcome!")
 
-// button.addEventListener("mouseleave",function(){
-//     console.log("CLICK!!!");
-// })
+let input = document.getElementById("userinput")
+// console.log(input)
+let button = document.getElementById("enter")
+// console.log(enter)
+let ul = document.querySelector("ul")
+// console.log(ul)
 
-var button = document.getElementById("enter");
-var input = document.getElementById("userinput");
-var ul = document.querySelector("ul");
 
-function inputLength() {
-	return input.value.length;
+function inputlength(){
+    return input.value.length
 }
 
-function createListElement() {
-    // console.log(input.value);
-	var li = document.createElement("li");
-	li.appendChild(document.createTextNode(input.value));
-	ul.appendChild(li);
-	input.value = "";
+function createListElement(){
+    let li = document.createElement("li") //creating a list element on click
+    li.appendChild(document.createTextNode(input.value)) //adding input value to the element
+    console.log(li.textContent)
+    ul.appendChild(li) //adding the newly created elemnet to the unordered list as a child
+    input.value = "" //making the previously entered input value vanish
+    console.log("New item added!")
 }
 
-function addListAfterClick() {
-	if (inputLength() > 0) {
-		createListElement();
-	}
+
+function addListByClick(){
+    if(inputlength() > 0){
+        createListElement()
+    }
 }
 
-function addListAfterKeypress(event) {
-	if (inputLength() > 0 && event.keyCode === 13) {
-		createListElement();
-	}
+function addListByKeyPress(event){
+    if(inputlength() > 0 && event.keyCode ===13){
+        createListElement()
+    }
 }
 
-button.addEventListener("click", addListAfterClick);
+button.addEventListener("click",addListByClick)
 
-input.addEventListener("keypress", addListAfterKeypress);
+input.addEventListener("keypress",addListByKeyPress)
