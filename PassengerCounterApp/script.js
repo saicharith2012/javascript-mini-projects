6b73327af9c5383d055ae50eb8086867f1b65873
saicharith2012 <saicharithpalabindhela@gmail.com>
2024-03-10 14:39:36 +0530

    adding decrement button and styles

--- PassengerCounterApp/script.js
+++ PassengerCounterApp/script.js
@@ -2,14 +2,13 @@
 let count = 0;
 
 let countEl = document.getElementById("count-el"); // passing in arguments
-let saveEl = document.getElementById("save-el");
+let saveContent = document.getElementById("save-content");
 
 // console.log(countEl)
 let numberOfClicks = 0;
 
 // listen for clicks on the increment button
 function increment() {
-  console.log("New passengers arrived...Welcome!");
 
   numberOfClicks += 1;
 
@@ -19,13 +18,23 @@ function increment() {
   // console.log(count)
 }
 
+function decrement() {
+  numberOfClicks -= 1;
+
+  // increment the count variable when the button is clicked
+  count -= 1;
+  countEl.innerText = count;
+  // console.log(count)
+}
+
 // saving the count
 function save() {
   let saveMessage = numberOfClicks + " | ";
-  saveEl.textContent += saveMessage;
+  saveContent.textContent += saveMessage;
   console.log(count);
   numberOfClicks = 0;
   //   console.log(saveMessage);
+  console.log("New passengers arrived...Welcome!");
 }
 
 // clears all the count and entries
@@ -38,5 +47,5 @@ function clearAll() {
   count = 0;
   countEl.textContent = count;
   console.log(count);
-  saveEl.textContent = "Previous Entries: ";
+  saveContent.textContent = "";
 }
